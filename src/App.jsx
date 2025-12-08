d6c9adb97c31649e471e7921bb5807067409b3e5
Titomr2025 <tmartinez2019@hotmail.com>
2025-12-07 19:52:37 -0500

    Se agregan formularios de Login y Register con validaciones y Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import './App.css'
 import Navbar from './Componentes/Navbar'
 import Home from './Componentes/Home'
 import Footer from './Componentes/Footer'
+import Register from './Componentes/Register'
+import Login from './Componentes/Login'
 
 function App() {
 
   return (
     <>
-    
        <Navbar></Navbar>
-       <Home></Home>
+       {/* <Home></Home> */}
+       {/* <Register></Register> */}
+       <Login></Login>
        <Footer></Footer>
-     
     </>
   )
 }
