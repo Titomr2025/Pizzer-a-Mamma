8d06c1f0af6137ea9b1c274cd141f87214353c5a
Titomr2025 <tmartinez2019@hotmail.com>
2025-12-21 17:02:05 -0500

    Hito 4: consumo de APIs en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css'
-import Cart from './Componentes/Cart'
+// import Cart from './Componentes/Cart'
 import Footer from './Componentes/Footer'
 import Navbar from './Componentes/Navbar'
 // import Home from './Componentes/Home'
 // import Login from './Componentes/Login'
 // import Register from './Componentes/Register'
+import Pizza from './Componentes/Pizza'
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
       <Navbar />
-      {/* <Home /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      <Cart />
+      <main className="main-content">
+        {/* <Home /> */}
+        {/* <Register /> */}
+        {/* <Login /> */}
+        {/* <Cart /> */}
+        <Pizza />
+      </main>
       <Footer />
     </div>
   )
--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import CardPizza from './CardPizza';
-import { pizzas } from './pizzas';
 
 const Home = () => {
+  const [pizzasList, setPizzasList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/pizzas')
+      .then(response => response.json())
+      .then(data => setPizzasList(data));
+  }, []);
+
   return (
     <div className="main-content">
       <Header img="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&auto=format&fit=crop&w=2070&q=80"></Header>
       
       <div className="container mt-4">
         <div className="row justify-content-center">
-          {pizzas.map((pizza) => (
+          {pizzasList.map((pizza) => (
             <div key={pizza.id} className="col-12 col-sm-6 col-md-4 mb-4 d-flex justify-content-center">
               <CardPizza
                 name={pizza.name}
